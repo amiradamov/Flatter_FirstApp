16d32e6f45d9aeab1bf15d4a39e22ac1691c5f15
amiradamov <adamov.aamir@gmail.com>
2021-11-23 11:40:24 +0300

    Flatbutton / RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,21 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image(
-          image: AssetImage('assets/pan.jpg'),
+        child: RaisedButton(
+          //Flatbutton
+          onPressed: () {
+            print('you are awesome');
+          },
+          child: Text('click me'),
+          color: Colors.lightGreen,
         ),
+        // child: Icon(Icons.air_sharp, color: Colors.lightBlue, size: 50.0),
+        // child: Image.asset('assets/barber.jpg'),
+        // child: Image.network('https://images.unsplash.com/photo-1637161286732-d94c874fadba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDUwfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click me'),
+        child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
     );
